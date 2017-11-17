56714bbf84145d5ab07716d31487b4877f3f77b7
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 15:13:24 -0600

    adds runnow task

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -2,6 +2,13 @@
 
 var gulp = require('gulp');
 
+gulp.task('runnow', function() {
+  var d = new Date();
+  var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+  // place code for your default task here
+  console.log('runnow '+timestamp);
+});
+
 gulp.task('default', function() {
   var d = new Date();
   var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
