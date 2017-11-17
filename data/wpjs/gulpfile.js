bb90fd2f5f066c9c17bae5f54d7e9f6d79db371d
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 15:47:24 -0600

    deletes taskname2

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -10,11 +10,4 @@ gulp.task('minify_css_and_copy', function() {
   console.log('Running Gulp minify_css_and_copy at '+timestamp);
 });
 
-gulp.task('taskname2', function() {
-  var d = new Date();
-  var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-  // place code for your default task here
-  console.log('Running Gulp taskname2 at '+timestamp);
-});
-
 gulp.task('default', ['minify_css_and_copy']);
