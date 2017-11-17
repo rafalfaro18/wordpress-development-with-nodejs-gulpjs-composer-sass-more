dad6d8b1d16aac9378856d8f106d74d43249bf6a
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 14:58:37 -0600

    adds timestamp to log

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
+  var d = new Date();
+  var timestamp = d.getHours() + ":" + d.getMinutes() + d.getSeconds();
   // place code for your default task here
-  console.log('running gulp');
+  console.log('running gulp'+timestamp);
 });
