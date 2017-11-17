3dca41818cfe8b28890dc24b2e65e2498cdcc71a
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 15:43:07 -0600

    changes task name to minify_css_and_copy

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -3,11 +3,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('taskname1', function() {
+gulp.task('minify_css_and_copy', function() {
   var d = new Date();
   var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
   // place code for your default task here
-  console.log('Running Gulp taskname1 at '+timestamp);
+  console.log('Running Gulp minify_css_and_copy at '+timestamp);
 });
 
-gulp.task('default', ['taskname1']);
+gulp.task('default', ['minify_css_and_copy']);
