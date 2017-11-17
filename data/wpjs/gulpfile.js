7d134bada74c34a1e6b899ab1692911fbed8a877
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 15:48:23 -0600

    updates task log

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('minify_css_and_copy', function() {
   var d = new Date();
   var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
   // place code for your default task here
-  console.log('Running Gulp minify_css_and_copy at '+timestamp);
+  console.log('Minifying css at '+timestamp);
 });
 
 gulp.task('default', ['minify_css_and_copy']);
