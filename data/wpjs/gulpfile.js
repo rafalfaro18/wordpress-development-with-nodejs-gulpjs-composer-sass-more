7e0fc995a5f1f81b17f283b290ce69287f45ee61
Rafael Alfaro <rafaela@thehangar.cr>
2017-11-17 15:24:06 -0600

    change runnow to taskname1

--- data/wpjs/gulpfile.js
+++ data/wpjs/gulpfile.js
@@ -3,11 +3,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('runnow', function() {
+gulp.task('taskname1', function() {
   var d = new Date();
   var timestamp = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
   // place code for your default task here
-  console.log('runnow '+timestamp);
+  console.log('taskname1 '+timestamp);
 });
 
-gulp.task('default', ['runnow']);
+gulp.task('default', ['taskname1']);
